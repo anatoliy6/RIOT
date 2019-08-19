7b9a66a05246d525e82b5b6134d571bb8072ec62
Anatoliy Atanasov <anatoliy@6lowpan.io>
2019-08-19 19:59:21 +0300

    drivers/bq27441: remove unnecessary function comments
    Signed-off-by: Anatoliy Atanasov <anatoliy@6lowpan.io>

--- drivers/bq27441/bq27441.c
+++ drivers/bq27441/bq27441.c
@@ -90,8 +90,6 @@ static int debug_print_i2c_error(int res)
     return 1;
 }
 
-
-// Initializes I2C and verifies communication with the BQ27441.
 bool bq27441_init(bq27441_t *dev, const bq27441_param_t *param)
 {
     assert(dev && param);
@@ -105,7 +103,6 @@ bool bq27441_init(bq27441_t *dev, const bq27441_param_t *param)
     return true;
 }
 
-// Configures the design capacity of the connected battery.
 bool bq27441_set_capacity(bq27441_t *dev, uint16_t capacity)
 {
     // Write to STATE subclass (82) of BQ27441 extended memory.
@@ -118,13 +115,11 @@ bool bq27441_set_capacity(bq27441_t *dev, uint16_t capacity)
     return _bq27441_write_extended_data(dev, BQ27441_ID_STATE, 10, capacityData, 2);
 }
 
-// Reads and returns the battery voltage
 uint16_t bq27441_get_voltage(const bq27441_t *dev)
 {
     return _bq27441_read_word(dev, BQ27441_COMMAND_VOLTAGE);
 }
 
-// Reads and returns the specified current measurement
 int16_t bq27441_get_current(const bq27441_t *dev, current_measure type)
 {
     int16_t current = 0;
@@ -144,7 +139,6 @@ int16_t bq27441_get_current(const bq27441_t *dev, current_measure type)
     return current;
 }
 
-// Reads and returns the specified capacity measurement
 uint16_t bq27441_get_capacity(const bq27441_t *dev, capacity_measure type)
 {
     uint16_t capacity = 0;
@@ -181,13 +175,11 @@ uint16_t bq27441_get_capacity(const bq27441_t *dev, capacity_measure type)
     return capacity;
 }
 
-// Reads and returns measured average power
 int16_t bq27441_get_power(const bq27441_t *dev)
 {
     return (int16_t)_bq27441_read_word(dev, BQ27441_COMMAND_AVG_POWER);
 }
 
-// Reads and returns specified state of charge measurement
 uint16_t bq27441_get_soc(const bq27441_t *dev, soc_measure type)
 {
     uint16_t socRet = 0;
@@ -204,7 +196,6 @@ uint16_t bq27441_get_soc(const bq27441_t *dev, soc_measure type)
     return socRet;
 }
 
-// Reads and returns specified state of health measurement
 uint8_t bq27441_get_soh(const bq27441_t *dev, soh_measure type)
 {
     uint16_t sohRaw = _bq27441_read_word(dev, BQ27441_COMMAND_SOH);
@@ -219,7 +210,6 @@ uint8_t bq27441_get_soh(const bq27441_t *dev, soh_measure type)
     }
 }
 
-// Reads and returns specified temperature measurement
 uint16_t bq27441_get_temperature(const bq27441_t *dev, temp_measure type)
 {
     uint16_t temp = 0;
@@ -235,10 +225,6 @@ uint16_t bq27441_get_temperature(const bq27441_t *dev, temp_measure type)
     return temp;
 }
 
-/*****************************************************************************
-************************** GPOUT Control Functions **************************
-*****************************************************************************/
-// Get GPOUT polarity setting (active-high or active-low)
 bool bq27441_get_gpout_polarity(const bq27441_t *dev)
 {
     uint16_t opConfigRegister = _bq27441_get_opConfig(dev);
@@ -246,7 +232,6 @@ bool bq27441_get_gpout_polarity(const bq27441_t *dev)
     return (opConfigRegister & BQ27441_OPCONFIG_GPIOPOL);
 }
 
-// Set GPOUT polarity to active-high or active-low
 bool bq27441_set_gpout_polarity(bq27441_t *dev, bool activeHigh)
 {
     uint16_t oldOpConfig = _bq27441_get_opConfig(dev);
@@ -268,7 +253,6 @@ bool bq27441_set_gpout_polarity(bq27441_t *dev, bool activeHigh)
     return _bq27441_write_op_config(dev, newOpConfig);
 }
 
-// Get GPOUT function (BAT_LOW or SOC_INT)
 bool bq27441_get_gpout_function(const bq27441_t *dev)
 {
     uint16_t opConfigRegister = _bq27441_get_opConfig(dev);
@@ -276,7 +260,6 @@ bool bq27441_get_gpout_function(const bq27441_t *dev)
     return (opConfigRegister & BQ27441_OPCONFIG_BATLOWEN);
 }
 
-// Set GPOUT function to BAT_LOW or SOC_INT
 bool bq27441_set_gpout_function(bq27441_t *dev, gpout_function function)
 {
     uint16_t oldOpConfig = _bq27441_get_opConfig(dev);
@@ -300,19 +283,16 @@ bool bq27441_set_gpout_function(bq27441_t *dev, gpout_function function)
     return _bq27441_write_op_config(dev, newOpConfig);
 }
 
-// Get SOC1_Set Threshold - threshold to set the alert flag
 uint8_t bq27441_get_soc1_set_threshold(bq27441_t *dev)
 {
     return _bq27441_read_extended_data(dev, BQ27441_ID_DISCHARGE, 0);
 }
 
-// Get SOC1_Clear Threshold - threshold to clear the alert flag
 uint8_t bq27441_get_soc1_clear_threshold(bq27441_t *dev)
 {
     return _bq27441_read_extended_data(dev, BQ27441_ID_DISCHARGE, 1);
 }
 
-// Set the SOC1 set and clear thresholds to a percentage
 bool bq27441_set_soc1_thresholds(bq27441_t *dev, uint8_t set, uint8_t clear)
 {
     uint8_t thresholds[2];
@@ -322,19 +302,16 @@ bool bq27441_set_soc1_thresholds(bq27441_t *dev, uint8_t set, uint8_t clear)
     return _bq27441_write_extended_data(dev, BQ27441_ID_DISCHARGE, 0, thresholds, 2);
 }
 
-// Get SOCF_Set Threshold - threshold to set the alert flag
 uint8_t bq27441_get_socf_set_threshold(bq27441_t *dev)
 {
     return _bq27441_read_extended_data(dev, BQ27441_ID_DISCHARGE, 2);
 }
 
-// Get SOCF_Clear Threshold - threshold to clear the alert flag
 uint8_t bq27441_get_socf_clear_threshold(bq27441_t *dev)
 {
     return _bq27441_read_extended_data(dev, BQ27441_ID_DISCHARGE, 3);
 }
 
-// Set the SOCF set and clear thresholds to a percentage
 bool bq27441_set_socf_thresholds(bq27441_t *dev, uint8_t set, uint8_t clear)
 {
     uint8_t thresholds[2];
@@ -344,7 +321,6 @@ bool bq27441_set_socf_thresholds(bq27441_t *dev, uint8_t set, uint8_t clear)
     return _bq27441_write_extended_data(dev, BQ27441_ID_DISCHARGE, 2, thresholds, 2);
 }
 
-// Check if the SOC1 flag is set
 bool bq27441_get_soc1_flag(const bq27441_t *dev)
 {
     uint16_t flagState = bq27441_get_flags(dev);
@@ -352,7 +328,6 @@ bool bq27441_get_soc1_flag(const bq27441_t *dev)
     return flagState & BQ27441_FLAG_SOC1;
 }
 
-// Check if the SOCF flag is set
 bool bq27441_get_socf_flag(const bq27441_t *dev)
 {
     uint16_t flagState = bq27441_get_flags(dev);
@@ -361,13 +336,11 @@ bool bq27441_get_socf_flag(const bq27441_t *dev)
 
 }
 
-// Get the SOC_INT interval delta
 uint8_t bq27441_get_soc_int_delta(bq27441_t *dev)
 {
     return _bq27441_read_extended_data(dev, BQ27441_ID_STATE, 26);
 }
 
-// Set the SOC_INT interval delta to a value between 1 and 100
 bool bq27441_set_soc_int_delta(bq27441_t *dev, uint8_t delta)
 {
     uint8_t soci = _constrain(delta, 0, 100);
@@ -375,20 +348,16 @@ bool bq27441_set_soc_int_delta(bq27441_t *dev, uint8_t delta)
     return _bq27441_write_extended_data(dev, BQ27441_ID_STATE, 26, &soci, 1);
 }
 
-// Pulse the GPOUT pin - must be in SOC_INT mode
 bool bq27441_pulse_gpout(const bq27441_t *dev)
 {
     return _bq27441_execute_control_word(dev, BQ27441_CONTROL_PULSE_SOC_INT);
 }
 
-// Read the device type - should be 0x0421
 uint16_t bq27441_get_device_type(const bq27441_t *dev)
 {
     return _bq27441_read_control_word(dev, BQ27441_CONTROL_DEVICE_TYPE);
 }
 
-// Enter configuration mode - set userControl if calling from an Arduino sketch
-// and you want control over when to exitConfig
 bool bq27441_enter_config_mode(bq27441_t *dev, bool userControl)
 {
     if (userControl) {
@@ -414,7 +383,6 @@ bool bq27441_enter_config_mode(bq27441_t *dev, bool userControl)
     return false;
 }
 
-// Exit configuration mode with the option to perform a resimulation
 bool bq27441_exit_config_mode(const bq27441_t *dev, bool resim)
 {
     // There are two methods for exiting config mode:
@@ -444,13 +412,11 @@ bool bq27441_exit_config_mode(const bq27441_t *dev, bool resim)
     }
 }
 
-// Read the flags() command
 uint16_t bq27441_get_flags(const bq27441_t *dev)
 {
     return _bq27441_read_word(dev, BQ27441_COMMAND_FLAGS);
 }
 
-// Read the CONTROL_STATUS subcommand of control()
 uint16_t bq27441_get_status(const bq27441_t *dev)
 {
     return _bq27441_read_control_word(dev, BQ27441_CONTROL_STATUS);
@@ -468,7 +434,6 @@ bool _bq27441_seal(const bq27441_t *dev)
     return _bq27441_read_control_word(dev, BQ27441_CONTROL_SEALED);
 }
 
-// UNseal the BQ27441-G1A
 bool _bq27441_unseal(const bq27441_t *dev)
 {
     // To unseal the BQ27441, write the key to the control
@@ -479,13 +444,11 @@ bool _bq27441_unseal(const bq27441_t *dev)
     return false;
 }
 
-// Read the 16-bit opConfig register from extended data
 uint16_t _bq27441_get_opConfig(const bq27441_t *dev)
 {
     return _bq27441_read_word(dev, BQ27441_EXTENDED_OPCONFIG);
 }
 
-// Write the 16-bit opConfig register in extended data
 bool _bq27441_write_op_config(bq27441_t *dev, uint16_t value)
 {
     uint8_t opConfigMSB = value >> 8;
@@ -496,13 +459,11 @@ bool _bq27441_write_op_config(bq27441_t *dev, uint16_t value)
     return _bq27441_write_extended_data(dev, BQ27441_ID_REGISTERS, 0, opConfigData, 2);
 }
 
-// Issue a soft-reset to the BQ27441-G1A
 bool _bq27441_soft_reset(const bq27441_t *dev)
 {
     return _bq27441_execute_control_word(dev, BQ27441_CONTROL_SOFT_RESET);
 }
 
-// Read a 16-bit command word from the BQ27441-G1A
 uint16_t _bq27441_read_word(const bq27441_t *dev, uint16_t subAddress)
 {
     uint8_t data[2];
@@ -511,7 +472,6 @@ uint16_t _bq27441_read_word(const bq27441_t *dev, uint16_t subAddress)
     return ((uint16_t)data[1] << 8) | data[0];
 }
 
-// Read a 16-bit subcommand() from the BQ27441-G1A's control()
 uint16_t _bq27441_read_control_word(const bq27441_t *dev, uint16_t function)
 {
     uint8_t subCommandMSB = (function >> 8);
@@ -528,7 +488,6 @@ uint16_t _bq27441_read_control_word(const bq27441_t *dev, uint16_t function)
     return false;
 }
 
-// Execute a subcommand() from the BQ27441-G1A's control()
 bool _bq27441_execute_control_word(const bq27441_t *dev, uint16_t function)
 {
     uint8_t subCommandMSB = (function >> 8);
@@ -544,7 +503,6 @@ bool _bq27441_execute_control_word(const bq27441_t *dev, uint16_t function)
     return false;
 }
 
-// Issue a BlockDataControl() command to enable BlockData access
 bool _bq27441_block_data_control(const bq27441_t *dev)
 {
     uint8_t enableByte = 0x00;
@@ -552,19 +510,16 @@ bool _bq27441_block_data_control(const bq27441_t *dev)
     return _bq27441_write_bytes_i2c(dev, BQ27441_EXTENDED_CONTROL, &enableByte, 1);
 }
 
-// Issue a DataClass() command to set the data class to be accessed
 bool _bq27441_block_data_class(const bq27441_t *dev, uint8_t id)
 {
     return _bq27441_write_bytes_i2c(dev, BQ27441_EXTENDED_DATACLASS, &id, 1);
 }
 
-// Issue a DataBlock() command to set the data block to be accessed
 bool _bq27441_block_data_offset(const bq27441_t *dev, uint8_t offset)
 {
     return _bq27441_write_bytes_i2c(dev, BQ27441_EXTENDED_DATABLOCK, &offset, 1);
 }
 
-// Read the current checksum using BlockDataCheckSum()
 uint8_t _bq27441_block_data_checksum(const bq27441_t *dev)
 {
     uint8_t csum;
@@ -573,7 +528,6 @@ uint8_t _bq27441_block_data_checksum(const bq27441_t *dev)
     return csum;
 }
 
-// Use BlockData() to read a byte from the loaded extended data
 uint8_t _bq27441_read_block_data(const bq27441_t *dev, uint8_t offset)
 {
     uint8_t ret;
@@ -583,7 +537,6 @@ uint8_t _bq27441_read_block_data(const bq27441_t *dev, uint8_t offset)
     return ret;
 }
 
-// Use BlockData() to write a byte to an offset of the loaded data
 bool _bq27441_write_block_data(const bq27441_t *dev, uint8_t offset, uint8_t data)
 {
     uint8_t address = offset + BQ27441_EXTENDED_BLOCKDATA;
@@ -591,8 +544,6 @@ bool _bq27441_write_block_data(const bq27441_t *dev, uint8_t offset, uint8_t dat
     return _bq27441_write_bytes_i2c(dev, address, &data, 1);
 }
 
-// Read all 32 bytes of the loaded extended data and compute a
-// checksum based on the values.
 uint8_t _bq27441_compute_block_checksum(const bq27441_t *dev)
 {
     uint8_t data[32];
@@ -608,13 +559,11 @@ uint8_t _bq27441_compute_block_checksum(const bq27441_t *dev)
     return csum;
 }
 
-// Use the BlockDataCheckSum() command to write a checksum value
 bool _bq27441_write_block_checksum(const bq27441_t *dev, uint8_t csum)
 {
     return _bq27441_write_bytes_i2c(dev, BQ27441_EXTENDED_CHECKSUM, &csum, 1);
 }
 
-// Read a byte from extended data specifying a class ID and position offset
 uint8_t _bq27441_read_extended_data(bq27441_t *dev, uint8_t classID, uint8_t offset)
 {
     uint8_t retData = 0;
@@ -645,8 +594,6 @@ uint8_t _bq27441_read_extended_data(bq27441_t *dev, uint8_t classID, uint8_t off
     return retData;
 }
 
-// Write a specified number of bytes to extended data specifying a
-// class ID, position offset.
 bool _bq27441_write_extended_data(bq27441_t *dev, uint8_t classID, uint8_t offset, uint8_t *data, uint8_t len)
 {
     if (len > 32) {
@@ -686,7 +633,6 @@ bool _bq27441_write_extended_data(bq27441_t *dev, uint8_t classID, uint8_t offse
     return true;
 }
 
-// Read a specified number of bytes over I2C at a given subAddress
 int16_t _bq27441_read_bytes_i2c(const bq27441_t *dev, uint8_t subAddress, uint8_t *dest, uint8_t count)
 {
     int16_t timeout = BQ72441_I2C_TIMEOUT;
@@ -707,7 +653,6 @@ int16_t _bq27441_read_bytes_i2c(const bq27441_t *dev, uint8_t subAddress, uint8_
     return timeout;
 }
 
-// Write a specified number of bytes over I2C to a given subAddress
 uint16_t _bq27441_write_bytes_i2c(const bq27441_t *dev, uint8_t subAddress, uint8_t *src, uint8_t count)
 {
     int res;
